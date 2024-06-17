34afe38e16f0ed82cc141001b075b7a1acc615d6
leibaio <leibaio@qq.com>
2024-06-17 23:07:55 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react";
+import { getList } from "@/api";
+import { useState } from "react";
+import "./App.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log(import.meta.env);
-  });
+
+  const handlePrimary = async () => {
+    const res = await getList({ query: 123 });
+    console.log("[ res ] >", res);
+  };
   return (
     <>
       <div>
@@ -30,7 +33,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-primary" onClick={handlePrimary}>
+        Primary
+      </button>
       <button className="btn btn-secondary">Secondary</button>
     </>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const config = loadEnv(mode, "./")
+  // 加载当前环境对应的环境变量配置
+  const env = loadEnv(mode, process.cwd())
+
   return {
     plugins: [react()], resolve: {
       alias: {
@@ -17,7 +18,7 @@ export default defineConfig(({ mode }) => {
       host: "localhost",
       proxy: {
         "/api": {
-          target: config.VITE_TARGET,  // 会自动判断当前环境，对应上述三个文件中的target
+          target: env.VITE_TARGET,  // 会自动判断当前环境，对应上述三个文件中的target
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
