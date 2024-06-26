4c695122f53791482a62fc1223805a9914e0bf30
leibaio <leibaio@qq.com>
2024-06-26 23:39:25 +0800

    feat: 接口

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,13 @@
 import request from "@/utils/request";
 
+export const login = (data: any) => {
+  return request({
+    url: "/api/login",
+    method: "post",
+    data,
+  });
+};
+
 export const getUserList = (params: any) => {
   return request({
     url: "/api/getUserList",
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,50 +1,58 @@
+import { Button, Form, Input, Checkbox } from "antd";
+import { login } from "@/api";
+
 const Login = () => {
-  const handleLogin = () => {
-    console.log("login");
+  const [form] = Form.useForm();
+  const layout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 18 },
+  };
+
+  const onRememberMe = (e: any) => {
+    console.log("[ e ] >", e);
+  };
+
+  const handleLogin = async () => {
+    const values = await form.validateFields();
+    console.log("[ values ] >", values);
+    await login(values);
   };
 
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <form
-          onSubmit={handleLogin}
+        <Form
           className="p-8 bg-white rounded-lg shadow-md w-96"
+          form={form}
+          {...layout}
         >
           <h2 className="text-2xl font-bold text-center mb-8">登录</h2>
-
-          <div className="mb-4">
-            <label className="block text-gray-700">邮箱</label>
-            <input
-              type="email"
-              placeholder="example@example.com"
-              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
-            />
-          </div>
-
-          <div className="mb-6">
-            <label className="block text-gray-700">密码</label>
-            <input
-              type="password"
-              placeholder="········"
-              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
-            />
-          </div>
-
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "请输入邮箱" }]}
+          >
+            <Input placeholder="请输入邮箱" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "请输入密码" }]}
+          >
+            <Input.Password placeholder="请输入密码" />
+          </Form.Item>
           <div className="flex items-center justify-between mb-6">
             <label className="flex items-center">
-              <input type="checkbox" className="form-checkbox" />
-              <span className="ml-2 text-sm text-gray-600">记住我</span>
+              <Checkbox onChange={onRememberMe}>记住我</Checkbox>
             </label>
-
-            <a href="#" className="text-sm text-indigo-500 hover:underline">
+            <a href="#" className="text-sm text-blue-500 hover:underline">
               忘记密码?
             </a>
           </div>
-
-          <button className="w-full p-3 text-white bg-indigo-500 hover:bg-indigo-600 rounded-md focus:outline-none focus:ring">
+          <Button className="w-full p-5" type="primary" onClick={handleLogin}>
             登录
-          </button>
-        </form>
+          </Button>
+        </Form>
       </div>
     </>
   );
