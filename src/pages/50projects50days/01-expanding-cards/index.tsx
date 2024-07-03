1c1cddb4d573b51ce159f7d3295b7c2d01d2c9f5
leibaio <leibaio@qq.com>
2024-07-03 22:25:57 +0800

    feat: expanding-cards

--- src/pages/50projects50days/01-expanding-cards/index.tsx
+++ src/pages/50projects50days/01-expanding-cards/index.tsx
@@ -1,10 +1,61 @@
-import './index.less'
+import "./index.less";
 
 const ExpandingCards = () => {
+  const panels = document.querySelectorAll(".panel");
+
+  panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+      removeActiveClasses();
+      panel.classList.add("active");
+      console.log("[ click ] >", panel);
+    });
+  });
+
+  function removeActiveClasses() {
+    panels.forEach((panel) => {
+      panel.classList.remove("active");
+    });
+  }
+
   return (
-    <>
-      <div>ExpandingCards page</div>
-    </>
+    <div className="expanding-cards-container">
+      <div
+        className="panel active"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')",
+        }}
+      >
+        <h3>Explore The World</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')",
+        }}
+      >
+        <h3>Explore The World</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')",
+        }}
+      >
+        <h3>Explore The World</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')",
+        }}
+      >
+        <h3>Explore The World</h3>
+      </div>
+    </div>
   );
 };
 
