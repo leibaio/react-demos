486e0ac013e795baba63e158dc85da911bf5c86a
leibaio <leibaio@qq.com>
2024-07-10 23:21:09 +0800

    feat: 全局鼠标移动图片变化

--- src/pages/metro/index.tsx
+++ src/pages/metro/index.tsx
@@ -1,43 +1,47 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import "./index.less";
 
 const Metro = () => {
   const imageRef = useRef<HTMLImageElement>(null);
 
-  const handleMouseMove = (event: React.MouseEvent) => {
-    console.log("[ event ] >", event);
-    if (imageRef.current) {
-      const { clientX, clientY } = event;
-      const { offsetWidth, offsetHeight, offsetLeft, offsetTop } =
-        imageRef.current;
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      if (imageRef.current) {
+        const { clientX, clientY } = event;
+        const { offsetWidth, offsetHeight, offsetLeft, offsetTop } =
+          imageRef.current;
 
-      const x = clientX - (offsetLeft + offsetWidth / 2);
-      const y = clientY - (offsetTop + offsetHeight / 2);
+        const x = clientX - (offsetLeft + offsetWidth / 2);
+        const y = clientY - (offsetTop + offsetHeight / 2);
 
-      const rotateX = (y / offsetHeight) * 15;
-      const rotateY = (x / offsetWidth) * -15;
+        const rotateX = (y / offsetHeight) * 10;
+        const rotateY = (x / offsetWidth) * -10;
 
-      imageRef.current.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-    }
-  };
+        imageRef.current.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+      }
+    };
 
-  const handleMouseLeave = () => {
-    if (imageRef.current) {
-      imageRef.current.style.transform = "rotateX(0deg) rotateY(0deg)";
-    }
-  };
+    const handleMouseLeave = () => {
+      if (imageRef.current) {
+        imageRef.current.style.transform = "rotateX(0deg) rotateY(0deg)";
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
 
   return (
     <div className="text-3xl">
       <div className="font-semibold text-3xl">metro</div>
       <div className="flex h-full bg-orange-300 text-3xl">
         <div className="w-2/3 flex flex-col p-1">
-          <div
-            className="rounded-xl image-container"
-            onMouseMove={handleMouseMove}
-            onMouseLeave={handleMouseLeave}
-            ref={imageRef}
-          >
+          <div className="rounded-xl image-container" ref={imageRef}>
             <img
               src="https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
               alt=""
