8dd4929fb57d3b21bcc9161b61ca9c1b03f595c5
leibaio <leibaio@qq.com>
2025-01-14 09:31:24 +0800

    feat: countdown

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState, useRef } from "react";
+
 const About = () => {
+  const [count, setCount] = useState(10);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setCount((prevCount) => {
+        if (prevCount <= 1) {
+          clearInterval(timerRef.current);
+          return 0;
+        }
+        return prevCount - 1;
+      });
+    }, 1000);
+
+    // 清除函数，在组件卸载时调用
+    return () => {
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+      }
+    };
+  }, []);
+
   return (
     <>
       <div>About page</div>
+      <h1>{count}</h1>
     </>
   );
 };
